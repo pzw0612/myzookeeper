a0f2bf2ba04e7be4891653f4e2f8e3b6d0232a22
up2up <pzw_0612@163.com>
2017-03-06 23:07:29 +0800

    first commit

---
+++ Delete_API_Sync_Usage.java
@@ -0,0 +1,45 @@
+package com.hust.grid.leesf.examples;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooDefs.Ids;
+
+public class Delete_API_Sync_Usage implements Watcher {
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+    private static ZooKeeper zk;
+
+    public static void main(String[] args) throws Exception {
+        String path = "/zk-book";
+        zk = new ZooKeeper("master:2181", 5000,new Delete_API_Sync_Usage());
+        connectedSemaphore.await();
+
+        zk.create(path, "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println("success create znode: " + path);
+        zk.create(path + "/c1", "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        System.out.println("success create znode: " + path + "/c1");
+        try {
+            zk.delete(path, -1);
+        } catch (Exception e) {
+            System.out.println("fail to delete znode: " + path);
+            System.out.println("event type:" + e.toString());
+        }
+
+        zk.delete(path + "/c1", -1);
+        System.out.println("success delete znode: " + path + "/c1");
+        zk.delete(path, -1);
+        System.out.println("success delete znode: " + path);
+
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+
+    public void process(WatchedEvent event) {
+        if (KeeperState.SyncConnected == event.getState()) {
+            if (EventType.None == event.getType() && null == event.getPath()) {
+                connectedSemaphore.countDown();
+            }
+        }
+    }
+}
